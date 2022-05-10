2c196aaf35d2d7c834b2971c4abbd827aa0ebd35
pouya karimian <karimian.pouya96@outlook.com>
2022-05-11 01:44:58 +0430

    Initial commit

---
+++ ResumableUploder.WebUI/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace ResumableUploder.WebUI.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
